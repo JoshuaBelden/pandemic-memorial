5bcbb9f7833f8d7d8dcdebe0ed271339db8fffb0
Joshua Belden <gitkraken@joshuabelden.com>
2020-04-04 14:22:18 -0700

    Added additional documentation

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-const db = config.get('mongoEndpoint');
+const uri = config.get('mongoEndpointUri');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, { 
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
             useFindAndModify: false
         });
-        console.log('Database connected');
+        console.log('Connection to MongoDB has been established.');
     } catch (err) {
-        console.error('error', err.message);
+        console.error('Error connecting to mongo.', err.message);
+
+        // If there are errors connecting to Mongo, then shut the entire
+        //  api down. There's no sense in limping along.
         process.exit(1);
     }
 };
--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,21 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Define middleware
+// MIDDLEWARE
+
 app.use(express.json({ extended: false }));
 app.options('*', cors());
 app.use(cors())
 
-// Define routes
+// ROUTES
+
 app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/articles', require('./routes/api/article'));
 
-// Connect Database
 connectDB();
 
 const PORT = process.env.PORT || 7000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
